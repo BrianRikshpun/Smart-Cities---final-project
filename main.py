318dfbe70851ebf1027574e1c41ea91dbf97a1a2
brianrikshpun <brianrik911@gmail.com>
2022-05-31 22:29:43 +0300

    2

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def startML():
+    WB = pd.read_csv("WB.csv")
+    WWB = pd.read_csv('WWB.csv')
+    WeatherAndWaterC = pd.read_csv('WeatherAndWaterDateC.csv')
 
+    print(WB.head())
+    print("------")
 
-def startML(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(WWB.head())
+    print("------")
 
+    print(WeatherAndWaterC.head())
+    print("------")
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    
+    startML()
 
-
-    startML('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
