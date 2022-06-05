cd4ed81480ec5a0b787d3e984ff01c426905d37c
Brian Rikshpun <brianrik911@gmail.com>
2022-06-05 22:44:31 +0300

    new

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 
 def startML():
@@ -11,14 +12,14 @@ def startML():
     WWB = pd.read_csv('WWB.csv') #Water and weather data
 
     #define the models
-    models = [LogisticRegression(), GaussianNB(), KNeighborsClassifier() ]
+    models = [LogisticRegression(), GaussianNB(), KNeighborsClassifier(), DecisionTreeClassifier(), RandomForestClassifier()]
 
     #define the search space
     space = [[{"penalty": ['none', 'l2'], "C": [0.5, 1.0, 5.0], "solver": ["newton-cg", "lbfgs", "sag"]}],
              [],
              [{"n_neighbors": [2, 3, 5], "algorithm": ["ball_tree", "kd_tree", "brute"], "p" : [1,2]}],
-             [{"n_estimators": [50, 70, 100], "max_depth": [3, 5]}],
-             [{"max_depth": [3, 5], "learning_rate": [0.1, 0.3], "n_estimators": [50, 70, 100]}]]
+             [{"criterion": ['gini', 'entropy', 'log_loss'], "max_depth": [3, 5 , 7], "min_samples_leaf" : [1,2,3] , "max_features" : ['auto', 'sqrt' , 'log2']}],
+             [{"max_depth": [3, 5],"max_depth": [3, 5 , 7], "criterion": ['gini', 'entropy', 'log_loss'], "n_estimators": [50, 70, 100]}]]
 
 
 
