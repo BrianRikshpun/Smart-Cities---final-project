9874832fbc2dc67f1c1a71d9171c3311fb98e95b
Brian Rikshpun <brianrik911@gmail.com>
2022-06-07 10:16:55 +0300

    CF

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from matplotlib import pyplot as plt
-
 from Preprocess import Preprocess
 from ClassicModels import ClassicModels
+from Visualization import Visualization
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
@@ -15,15 +15,13 @@ def startML():
     WWB = pd.read_csv('WWB.csv') #Water and weather data
 
     #define the models
-    models = [LogisticRegression(), GaussianNB(), KNeighborsClassifier(), DecisionTreeClassifier(), RandomForestClassifier()]
+    models = [LogisticRegression(), KNeighborsClassifier(), DecisionTreeClassifier(), RandomForestClassifier()]
 
     #define the search space
     space = [[{"penalty": ['none', 'l2'], "C": [0.5, 1.0, 5.0], "solver": ["newton-cg", "lbfgs", "sag"]}],
-             [],
              [{"n_neighbors": [2, 3, 5], "algorithm": ["ball_tree", "kd_tree", "brute"], "p" : [1,2]}],
              [{"criterion": ['gini', 'entropy', 'log_loss'], "max_depth": [3, 5 , 7], "min_samples_leaf" : [1,2,3] , "max_features" : ['auto', 'sqrt' , 'log2']}],
-             [{"max_depth": [3, 5],"max_depth": [3, 5 , 7], "criterion": ['gini', 'entropy', 'log_loss'], "n_estimators": [50, 70, 100]}]]
-
+             [{"max_depth": [3, 5],"max_depth": [3, 5 , 7], "criterion": ['gini', 'entropy'], "n_estimators": [50, 70, 100]}]]
 
 
     pre_WB = Preprocess(WB)
@@ -32,16 +30,12 @@ def startML():
     pre_WWB = Preprocess(WWB)
     X_train_WWB, X_test_WWB, y_train_WWB, y_test_WWB = pre_WWB.SplitPcaScale(WWB)
 
+    ModelsF = ClassicModels(models, space, X_train_WB, X_test_WB, y_train_WB, y_test_WB)
+    res_data = ModelsF.FindBestParams(models, space, X_train_WB, X_test_WB, y_train_WB, y_test_WB)
 
-    res_data = ClassicModels(pre_WB)
-    x_for_plot = ["LogisticRegression", "GaussianNB", "KNeighborsClassifier", "DecisionTreeClassifier", "RandomForestClassifier"]
-    y_for_plot = res_data[0]
-
-    plt.bar(x_for_plot, y_for_plot, width=0.4)
-    plt.xlabel("Model")
-    plt.ylabel("AUC")
-    plt.title("AUC for each model - TEST")
-    plt.show()
+    Visualizations = Visualization(res_data)
+    Visualizations.ShowAUC(res_data)
+    Visualizations.ShowConfussionMatrix(res_data)
 
 
 if __name__ == '__main__':
