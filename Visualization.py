ad078f22346a6e8eceb49f0899aedf7a0504f3e9
Brian Rikshpun <brianrik911@gmail.com>
2022-06-07 11:11:21 +0300

    with vis

--- Visualization.py
+++ Visualization.py
@@ -4,11 +4,12 @@ from matplotlib import pyplot as plt
 class Visualization:
     def __init__(self, res_data):
        self.res_data = res_data
+       self.models = ["LogisticRegression", "KNeighborsClassifier", "DecisionTreeClassifier", "RandomForestClassifier"]
 
 
     def ShowAUC(self, res_data):
 
-        x_for_plot = ["LogisticRegression", "KNeighborsClassifier", "DecisionTreeClassifier", "RandomForestClassifier"]
+        x_for_plot = self.models
         y_for_plot = res_data[0]
 
         def addlabels(x, y):
@@ -36,3 +37,17 @@ class Visualization:
             plt.ylabel('Actuals', fontsize=18)
             plt.title('Confusion Matrix', fontsize=18)
             plt.show()
+
+
+    def ShowRoc(self, res_data):
+        counter = 0
+        for i in res_data[3]:
+            # create ROC curve
+            plt.plot(i[0], i[1], label = "Model " + self.models[counter] + " AUC = " + str(res_data[0][counter]))
+            plt.ylabel('True Positive Rate')
+            plt.xlabel('False Positive Rate')
+            plt.legend(loc = 4)
+            plt.show()
+            counter += 1
+
+
