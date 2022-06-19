187159a9f086fefc2afff21e633cc6ff8d5b475b
almogklein <almogklein.16@gmail.com>
2022-06-19 23:40:30 +0300

    lstm_update_ main update

--- LSTM.py
+++ LSTM.py
@@ -8,6 +8,8 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense, RepeatVector, TimeDistributed
 import matplotlib.pyplot as plt
 import math
+from sklearn.preprocessing import MinMaxScaler
+from pandas import Series
 from tensorflow.keras.optimizers import SGD, Adam, Adamax, Adadelta, Adagrad
 
 
@@ -18,28 +20,33 @@ class LSTM_Model:
 
     def load_model(self, n_features):
         model = Sequential()
-        model.add(LSTM(50, activation='relu', recurrent_activation='sigmoid', return_sequences=True,
-                       input_shape=(self.n_steps_in, n_features)))
+        model.add(LSTM(50, activation='relu', recurrent_activation='sigmoid', return_sequences=True, input_shape=(self.n_steps_in, n_features)))
         model.add(LSTM(50, activation='relu', recurrent_activation='sigmoid', ))
         model.add(Dense(self.n_steps_out))
         # model.add(Dense(1)) ## one output
 
-        optimizers = [tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07, ),
-                      tf.keras.optimizers.Adagrad(learning_rate=0.001, initial_accumulator_value=0.1, epsilon=1e-07),
-                      tf.keras.optimizers.Adamax(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07),
-                      tf.keras.optimizers.Adadelta(learning_rate=0.001, rho=0.95, epsilon=1e-07)]
+        # model = Sequential()
+        # model.add(LSTM(100, activation='relu', input_shape=(self.n_steps_in, n_features)))
+        # model.add(RepeatVector(self.n_steps_out))
+        # model.add(LSTM(100, activation='relu', return_sequences=True))
+        # model.add(TimeDistributed(Dense(1)))
+
+        optimizers = tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07)
+        # tf.keras.optimizers.Adagrad(learning_rate=0.001, initial_accumulator_value=0.1, epsilon=1e-07),
+        # tf.keras.optimizers.Adamax(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07),
+        # tf.keras.optimizers.Adadelta(learning_rate=0.001, rho=0.95, epsilon=1e-07)]
         # tf.keras.optimizers.SGD(learning_rate=0.001, momentum=0.05, nesterov=False)]
 
         return model, optimizers
 
     def plot_tarin(self, optimizers, model, x_trin, x_test, x_val, y_trin, y_test, y_val):
-        op = ['adam', 'adagrad', 'adamax', 'adadelta']  # , 'sgd']
+        op = ['adam']  #, 'adagrad', 'adamax', 'adadelta' , 'sgd']
         i = 0
         for optimizer in optimizers:
             model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['accuracy', 'mse'])
             # fit network
-            history = model.fit(x_trin, y_trin, epochs=50, batch_size=1, validation_data=(x_test, y_test), verbose=2, shuffle=False)
-            model.evaluate(x_val, y_val, batch_size=1, return_dict=True)
+            history = model.fit(x_trin, y_trin, epochs=10, batch_size=7, validation_data=(x_test, y_test), verbose=0, shuffle=False)
+            model.evaluate(x_val, y_val, batch_size=7, return_dict=True)
             # summarize history for loss
             plt.figure()
             plt.plot(history.history['loss'], label='train')
@@ -49,6 +56,8 @@ class LSTM_Model:
             plt.xlabel('epoch')
             plt.legend()
             i += 1
+
+
 
     def binary_acc(self, y_pred, y_test):
         y_pred_tag = torch.round(torch.sigmoid(y_pred))
@@ -68,38 +77,114 @@ class LSTM_Model:
         in_seq1 = X['Water Temperature'].to_numpy()
         in_seq2 = X['Wave Height'].to_numpy()
         in_seq3 = X['Wave Period'].to_numpy()
-        in_seq4 = X['Battery Life'].to_numpy()
-        in_seq5 = X['Measurement Day'].to_numpy()
-        in_seq6 = X['Measurement Month'].to_numpy()
-        in_seq7 = X['Measurement Year'].to_numpy()
-        in_seq8 = X['Measurement Hour'].to_numpy()
+        in_seq4 = X['Battery Life'].to_numpy().astype(int)
+        in_seq5 = X['Measurement Day'].to_numpy().astype(int)
+        in_seq6 = X['Measurement Month'].to_numpy().astype(int)
+        in_seq7 = X['Measurement Year'].to_numpy().astype(int)
+        in_seq8 = X['Measurement Hour'].to_numpy().astype(int)
         in_seq9 = X['Latitude'].to_numpy()
-        in_seq10 = X['class'].to_numpy()
-        out_seq = X['Turbidity'].to_numpy()
+        in_seq10 = X['class'].to_numpy().astype(int)
+        out_seq = X['Turbidity'].to_numpy().astype(int)
 
-        # convert to [rows, columns] structure
-        in_seq1 = in_seq1.reshape((len(in_seq1), 1))
-        in_seq2 = in_seq2.reshape((len(in_seq2), 1))
-        in_seq3 = in_seq3.reshape((len(in_seq3), 1))
-        in_seq4 = in_seq4.reshape((len(in_seq4), 1))
-        in_seq5 = in_seq5.reshape((len(in_seq5), 1))
-        in_seq6 = in_seq6.reshape((len(in_seq6), 1))
-        in_seq7 = in_seq7.reshape((len(in_seq7), 1))
-        in_seq8 = in_seq8.reshape((len(in_seq8), 1))
-        in_seq9 = in_seq9.reshape((len(in_seq9), 1))
-        in_seq10 = in_seq10.reshape((len(in_seq10), 1))
-        out_seq = out_seq.reshape((len(out_seq), 1))
+        # # convert to [rows, columns] structure
+        # in_seq1 = in_seq1.reshape((len(in_seq1), 1))
+        # in_seq2 = in_seq2.reshape((len(in_seq2), 1))
+        # in_seq3 = in_seq3.reshape((len(in_seq3), 1))
+        # in_seq4 = in_seq4.reshape((len(in_seq4), 1))
+        # in_seq5 = in_seq5.reshape((len(in_seq5), 1))
+        # in_seq6 = in_seq6.reshape((len(in_seq6), 1))
+        # in_seq7 = in_seq7.reshape((len(in_seq7), 1))
+        # in_seq8 = in_seq8.reshape((len(in_seq8), 1))
+        # in_seq9 = in_seq9.reshape((len(in_seq9), 1))
+        # in_seq10 = in_seq10.reshape((len(in_seq10), 1))
+        # out_seq = out_seq.reshape((len(out_seq), 1))
         # horizontally stack columns
+
+        series_0 = Series(out_seq)
+        values0 = series_0.values
+        values0 = values0.reshape((len(values0), 1))
+        scaler_0 = MinMaxScaler(feature_range=(0, 1))
+        scaler_0 = scaler_0.fit(values0)
+        out_seq = scaler_0.transform(values0)
+
+        series_1 = Series(in_seq1)
+        values1 = series_1.values
+        values1 = values1.reshape((len(values1), 1))
+        scaler1 = MinMaxScaler(feature_range=(0, 1))
+        scaler1 = scaler1.fit(values1)
+        in_seq1 = scaler1.transform(values1)
+
+        series_2 = Series(in_seq2)
+        values2 = series_2.values
+        values2 = values2.reshape((len(values2), 1))
+        scaler2 = MinMaxScaler(feature_range=(0, 1))
+        scaler2 = scaler2.fit(values2)
+        in_seq2 = scaler2.transform(values2)
+
+        series_3 = Series(in_seq3)
+        values3 = series_3.values
+        values3 = values3.reshape((len(values3), 1))
+        scaler3 = MinMaxScaler(feature_range=(0, 1))
+        scaler3 = scaler3.fit(values3)
+        in_seq3 = scaler3.transform(values3)
+
+        series_4 = Series(in_seq4)
+        values4 = series_4.values
+        values4 = values4.reshape((len(values4), 1))
+        scaler4 = MinMaxScaler(feature_range=(0, 1))
+        scaler4 = scaler4.fit(values4)
+        in_seq4 = scaler4.transform(values4)
+
+        series5 = Series(in_seq5)
+        values5 = series5.values
+        values5 = values5.reshape((len(values5), 1))
+        scaler5 = MinMaxScaler(feature_range=(0, 1))
+        scaler5 = scaler5.fit(values5)
+        in_seq5 = scaler5.transform(values5)
+
+        series6 = Series(in_seq6)
+        values6 = series6.values
+        values6 = values6.reshape((len(values6), 1))
+        scaler6 = MinMaxScaler(feature_range=(0, 1))
+        scaler6 = scaler6.fit(values6)
+        in_seq6 = scaler6.transform(values6)
+
+        series7 = Series(in_seq7)
+        values7 = series7.values
+        values7 = values7.reshape((len(values7), 1))
+        scaler7 = MinMaxScaler(feature_range=(0, 1))
+        scaler7 = scaler7.fit(values7)
+        in_seq7 = scaler7.transform(values7)
+
+        series8 = Series(in_seq8)
+        values8 = series8.values
+        values8 = values8.reshape((len(values8), 1))
+        scaler8 = MinMaxScaler(feature_range=(0, 1))
+        scaler8 = scaler8.fit(values8)
+        in_seq8 = scaler8.transform(values8)
+
+        series9 = Series(in_seq9)
+        values9 = series9.values
+        values9 = values9.reshape((len(values9), 1))
+        scaler9 = MinMaxScaler(feature_range=(-1, 0))
+        scaler9 = scaler9.fit(values9)
+        in_seq9 = scaler9.transform(values9)
+
+        series10 = Series(in_seq10)
+        values10 = series10.values
+        values10 = values10.reshape((len(values10), 1))
+        scaler10 = MinMaxScaler(feature_range=(0, 1))
+        scaler10 = scaler10.fit(values10)
+        in_seq10 = scaler10.transform(values10)
+
         dataset = hstack((in_seq1, in_seq2, in_seq3, in_seq4, in_seq5, in_seq6, in_seq7, in_seq8, in_seq9, in_seq10, out_seq))
         # covert into input/output
-        x_trin, x_val, x_test, y_trin, y_val, y_test = self.split_sequences(dataset, self.n_steps_in, self.n_steps_out)
+        x_trin, x_val, x_test, y_trin, y_val, y_test, X_tv, y_tv = self.split_sequences(dataset, self.n_steps_in, self.n_steps_out)
         # the dataset knows the number of features, e.g. 2
         n_features = x_trin.shape[2]
-        print(x_trin.shape)
-        print(x_val.shape)
-        print(x_test.shape)
 
-        return x_trin, x_val, x_test, y_trin, y_val, y_test, n_features
+
+        return x_trin, x_val, x_test, y_trin, y_val, y_test, X_tv, y_tv, n_features, scaler_0
 
     # split a multivariate sequence into samples
     def split_sequences(self, sequences, n_steps_in, n_steps_out):
@@ -117,12 +202,15 @@ class LSTM_Model:
             y.append(seq_y)
         X = array(X)
         y = array(y)
-        s = math.floor(X.shape[0] * 0.7)  # round down
-        x_trin = X[:math.floor(s * 0.8)]
-        y_trin = y[:math.floor(s * 0.8)]
-        x_val = X[math.floor(s * 0.8):s]
-        y_val = y[math.floor(s * 0.8):s]
+
+        s = math.floor(X.shape[0] * 0.85)  # round down
+        x_trin = X[:math.floor(s * 0.85)]
+        y_trin = y[:math.floor(s * 0.85)]
+        x_val = X[math.floor(s * 0.85):s]
+        y_val = y[math.floor(s * 0.85):s]
         x_test = X[s:]
         y_test = y[s:]
+        X_tv = X[:s]
+        y_tv = y[:s]
 
-        return x_trin, x_val, x_test, y_trin, y_val, y_test
+        return x_trin, x_val, x_test, y_trin, y_val, y_test, X_tv, y_tv
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from pandas import DataFrame
 from matplotlib import pyplot as plt
 from Preprocess import Preprocess
 from ClassicModels import ClassicModels
@@ -7,19 +9,136 @@ from Data_Loader import prep_data_loder
 from nn_b_c import set_model
 from LSTM import LSTM_Model
 from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB
+# from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 import torch
+import math
 from sklearn.metrics import confusion_matrix, classification_report
+
 
 def start_lstm():
 
-    lstm = LSTM_Model(n_steps_in=2, n_steps_out=2)
-    x_trin, x_val, x_test, y_trin, y_val, y_test, n_features = lstm.prep_data()
-    model, optimizers = lstm.load_model(n_features=n_features)
-    lstm.plot_tarin(optimizers=optimizers, model=model, x_trin=x_trin, x_test=x_test, x_val=x_val, y_trin=y_trin, y_test=y_test, y_val=y_val)
+    n_steps_in_r = 8
+    n_steps_out_r = 5
+    epochs = 10
+    hh = []
+    prec = []
+    df = pd.read_csv('WB_0.csv')
+    ture_t = df['class'].astype(int)
+    s = math.floor(df.shape[0] * 0.85)
+    ture_t = ture_t[s:]
+    ddf = pd.DataFrame()
+
+
+    ttt1 = 0
+    ttt0 = 0
+    for c in ture_t:
+        # print(c)
+        if c == 1:
+            ttt1 += 1
+        else:
+            ttt0 += 1
+
+    print(ttt1, ttt0, len(ture_t))
+
+    for n_steps_out in range(2, n_steps_out_r+1):
+        for n_steps_in in range(2, n_steps_in_r+1):
+            if n_steps_in >= n_steps_out:
+                lstm = LSTM_Model(n_steps_in=n_steps_in, n_steps_out=n_steps_out)
+                x_trin, x_val, x_test, y_trin, y_val, y_test, X_tv, y_tv, n_features, scaler_0 = lstm.prep_data()
+                model, optimizers = lstm.load_model(n_features=n_features)
+                model.compile(optimizer=optimizers, loss='mean_squared_error', metrics=['accuracy', 'mse'])
+
+                # fit network
+
+                history = model.fit(x_trin, y_trin, epochs=epochs, batch_size=1, validation_data=(x_test, y_test), verbose=0, shuffle=False)
+                # history.appenmodel.evaluate(x_val, y_val, batch_size=1, return_dict=True)
+
+                # evaluate network
+                hh.append(history)
+
+                # plt.figure()
+                plt.plot(history.history['loss'], label=f'train {n_steps_in} {n_steps_out-1}')
+                plt.plot(history.history['val_loss'], label=f'test {n_steps_in} {n_steps_out-1}')
+                plt.title(f'model loss adam learn_in: {n_steps_in} ||| predict_out: {n_steps_out-1}')
+                plt.ylabel('loss')
+                plt.xlabel('epoch')
+                plt.legend()
+                plt.tight_layout()
+                # y_pred = model(x_test)
+
+                yhat = model.predict(x_test, verbose=0)
+                # yhat = model.predict_on_batch(x_test)
+                # print(yhat)
+                pre = []
+                for p in yhat:
+                    pp = []
+                    # print(p)
+                    for i in p:
+                        pp.append(scaler_0.inverse_transform(i.reshape(1, -1)).tolist()[0])
+                    pre.append(pp)
+
+                tp = 0
+                tn = 0
+                fp = 0
+                fn = 0
+                i = 0
+                for p in pre:
+                    cc = 0
+                    if i < (len(ture_t) - n_steps_out):
+                        if p[n_steps_out-1][0]*2 >= 5 and ture_t.iloc[i+n_steps_out] == 1:
+                            tp += 1
+                        if p[n_steps_out-1][0]*2 >= 5 and ture_t.iloc[i+n_steps_out] == 0:
+                            fn += 1
+                        if p[n_steps_out-1][0]*2 < 5 and ture_t.iloc[i+n_steps_out] == 1:
+                            fp += 1
+                        if p[n_steps_out-1][0]*2 < 5 and ture_t.iloc[i+n_steps_out] == 0:
+                            tn += 1
+                    #
+                    # for pp in p:
+                    #     # print(pp)
+                    #     if i < (len(ture_t) - n_steps_out):
+                    #         if pp[0] >= 5 and ture_t[i: i+n_steps_out].to_numpy()[cc] == 1:
+                    #             tp += 1
+                    #             # print("class 1")
+                    #         if pp[0] >= 5 and ture_t[i: i+n_steps_out].to_numpy()[cc] == 0:
+                    #             fn += 1
+                    #         if pp[0] < 5 and ture_t[i: i+n_steps_out].to_numpy()[cc] == 1:
+                    #             fp += 1
+                    #         if pp[0] < 5 and ture_t[i: i+n_steps_out].to_numpy()[cc] == 0:
+                    #             tn += 1
+                    #     cc+=1
+                    i+=1
+
+                # print('------------------------')
+                if tp == 0 and fn == 0 or tp == 0 and fp == 0:
+                    print("tp-11, fp-10, tn-00, fn-01")
+                    print(" ", tp, "   ", fp, "   ", tn, "    ", fn)
+                    senseti = -1.0
+                    preci = -1.0
+                else:
+                    print("tp-11, fp-10, tn-00, fn-01")
+                    print(" ", tp, "   ", fp, "   ",  tn, "    ",  fn)
+                    senseti = tp / (tp + fn)
+                    preci = tp / (tp + fp)
+                prec.append([n_steps_in, n_steps_out-1, senseti, preci, tp, fp, tn, fn])
+                ddf = ddf.append({'in': n_steps_in, 'out': n_steps_out-1, 'TP': tp, 'FP': fp, 'TN': tn, 'FN': fn}, ignore_index=True)
+            else:
+                pass
+    ddf.to_csv('wb_lstm.csv')
+    plt.figure()
+    for i in range(len(prec)):
+        plt.bar(i+1, prec[i][2], label=f'senseti {prec[i][0]} -> {prec[i][1]}')
+        plt.bar(i+1, prec[i][3], label=f'preci {prec[i][0]} -> {prec[i][1]}')
+        plt.ylabel('')
+        plt.xlabel('epx')
+        plt.legend()
+        plt.tight_layout()
+        print(f'{prec[i][0]} -> {prec[i][1]}')
+        print('senseti -- prec -- tp fp tn fn')
+        print(prec[i][:])
     plt.show()
 
 
@@ -84,9 +203,11 @@ def stat_nn():
 def startML():
 
     WB = pd.read_csv("WB_0.csv") #Water data
-
     WWB = pd.read_csv('wWB_0.csv') #Water and weather data
     fig_size = (10, 8)
+
+    ShiftsWB = []
+    ShiftsWWB = []
 
     #define the models
     models = [LogisticRegression(), KNeighborsClassifier(), DecisionTreeClassifier(), RandomForestClassifier()]
@@ -98,32 +219,49 @@ def startML():
              [{"max_depth": [3, 5],"max_depth": [3, 5 , 7], "criterion": ['gini', 'entropy'], "n_estimators": [50, 70, 100]}]]
 
 
-    pre_WB = Preprocess()
-    X_train1_WB, X_test_WB, y_train1_WB, y_test_WB = pre_WB.SplitPcaScale(WB, 'WB', fisi=fig_size)
-    X_train_WB, y_train_WB = pre_WB.smote(X_train1_WB, y_train1_WB)
-    pre_WB.plot_2d_space(X_train_WB, y_train_WB, X_train1_WB, y_train1_WB, label='SMOTE', fisi=fig_size)
+    for i in range(-7,0): #shift the class between -7 to -1
+        ShiftsWB.append(makeShifts(WB, i))
+        ShiftsWWB.append(makeShifts(WWB, i))
 
-    ModelsF = ClassicModels(models, space, X_train_WB, X_test_WB, y_train_WB, y_test_WB)
-    res_data = ModelsF.FindBestParams(models, space, X_train_WB, X_test_WB, y_train_WB, y_test_WB)
 
-    Visualizations1 = Visualization(res_data, fig_size)
-    Visualizations1.ShowAUC("WB")
-    Visualizations1.Show_Confussion_Matrix(X_test_WB, y_test_WB)
-    Visualizations1.ShowRoc("WB")
+    for i in range(7):
+        shift = (i + 1)*-1
+        pre_WB = Preprocess()
+        X_train1_WB, X_test_WB, y_train1_WB, y_test_WB = pre_WB.SplitPcaScale(ShiftsWB[i], 'WB shift = ' + str(shift) , fisi=fig_size)
+        X_train_WB, y_train_WB = pre_WB.smote(X_train1_WB, y_train1_WB)
+        pre_WB.plot_2d_space(X_train_WB, y_train_WB, X_train1_WB, y_train1_WB, label='SMOTE', fisi=fig_size)
 
-    pre_WWB = Preprocess()
-    X_train1_WWB, X_test_WWB, y_train1_WWB, y_test_WWB = pre_WWB.SplitPcaScale(WWB, 'WWB', fisi=fig_size)
-    X_train_WWB, y_train_WWB = pre_WWB.smote(X_train1_WWB, y_train1_WWB)
-    pre_WWB.plot_2d_space(X_train_WWB, y_train_WWB, X_train1_WWB, y_train1_WWB, label='SMOTE', fisi=fig_size)
+        ModelsF = ClassicModels(models, space, X_train_WB, X_test_WB, y_train_WB, y_test_WB)
+        res_data = ModelsF.FindBestParams(models, space, X_train_WB, X_test_WB, y_train_WB, y_test_WB)
 
-    ModelsF = ClassicModels(models, space, X_train_WWB, X_test_WWB, y_train_WWB, y_test_WWB)
-    res_data = ModelsF.FindBestParams(models, space, X_train_WWB, X_test_WWB, y_train_WWB, y_test_WWB)
+        Visualizations1 = Visualization(res_data, fig_size)
+        Visualizations1.ShowAUC('WB shift = ' + str(shift))
+        Visualizations1.Show_Confussion_Matrix(X_test_WB, y_test_WB)
+        Visualizations1.ShowRoc('WB shift = ' + str(shift))
 
-    Visualizations2 = Visualization(res_data, fig_size)
-    Visualizations2.ShowAUC("WWB")
-    Visualizations2.Show_Confussion_Matrix(X_test_WWB, y_test_WWB)
-    Visualizations2.ShowRoc("WWB")
-    plt.show()
+        #----------WWB------------
+        pre_WWB = Preprocess()
+        X_train1_WWB, X_test_WWB, y_train1_WWB, y_test_WWB = pre_WWB.SplitPcaScale(ShiftsWWB[i], 'WWB shift = ' + str(shift), fisi=fig_size)
+        X_train_WWB, y_train_WWB = pre_WWB.smote(X_train1_WWB, y_train1_WWB)
+        pre_WWB.plot_2d_space(X_train_WWB, y_train_WWB, X_train1_WWB, y_train1_WWB, label='SMOTE', fisi=fig_size)
+
+        ModelsF = ClassicModels(models, space, X_train_WWB, X_test_WWB, y_train_WWB, y_test_WWB)
+        res_data = ModelsF.FindBestParams(models, space, X_train_WWB, X_test_WWB, y_train_WWB, y_test_WWB)
+
+        Visualizations2 = Visualization(res_data, fig_size)
+        Visualizations2.ShowAUC('WWB shift = ' + str(shift))
+        Visualizations2.Show_Confussion_Matrix(X_test_WWB, y_test_WWB)
+        Visualizations2.ShowRoc('WWB shift = ' + str(shift))
+        plt.show()
+
+
+def makeShifts(data, n):
+    t = data
+    t['class'] = t['class'].shift(n).dropna().astype(int)
+    t = t.dropna()
+    t['class'] = t['class'].astype(int)
+
+    return t
 
 
 if __name__ == '__main__':
